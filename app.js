34dabe9f491855d5a245731f1a67c7dfc1af72d6
Aman11102005 <aj652708@gmail.com>
2025-03-29 12:02:12 +0530

    local to git

---
+++ app.js
@@ -0,0 +1,9 @@
+// let firstBtn = document.querySelector(".firstBtn")
+// let homePage = document.querySelector(".homePage")
+// let secondPage = document.querySelector(".secondPage")
+
+// firstBtn.addEventListener("click",()=>{
+//    homePage.classList.add("hide")
+//    secondPage.classList.remove("hide")
+
+// })
